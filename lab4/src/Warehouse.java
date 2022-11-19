312b57a40d00967381bed0025abad2b1995ac60f
SzymonWesierski <sz425@outlook.com>
2022-11-19 18:34:38 +0100

    lab4

--- lab4/src/Warehouse.java
+++ lab4/src/Warehouse.java
@@ -4,8 +4,13 @@ public class Warehouse {
     public Product[] getProduct() {
         return productlist;
     }
-    
+
     public Warehouse(int number){
         this.productlist = new Product[number];
+
+        for(int i = 0; i < number; i++){
+            this.productlist[i] = null;
+        }
+
     }
 }
